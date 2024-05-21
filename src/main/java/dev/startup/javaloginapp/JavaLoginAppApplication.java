230da36d4e83c7bbada1bc30f1a799f1f01b1135
mma <mma@mmas-MacBook-Pro.local>
2024-05-21 20:48:07 +0700

    project initialization

---
+++ src/main/java/dev/startup/javaloginapp/JavaLoginAppApplication.java
@@ -0,0 +1,13 @@
+package dev.startup.javaloginapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLoginAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLoginAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/startup/javaloginapp/JavaLoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.startup.javaloginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLoginAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
